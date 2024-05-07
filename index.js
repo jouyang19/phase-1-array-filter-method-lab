a459c4e14b0dc9abe9ccc71067abe5aaa7179567
Guang <jouyang19@proton.me>
2024-05-07 13:49:46 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, attribute) {
+  const newArr = array.filter(
+    (name) =>
+      name === attribute || name.toUpperCase() === attribute.toUpperCase()
+  );
+  return newArr;
+}
+
+function fuzzyMatch(array, attribute) {
+  const newArr = array.filter(
+    (name) => name.slice(0, attribute.length) === attribute
+  );
+  return newArr;
+}
+
+function matchName(array, attribute) {
+  return array.filter((element) => element.name === attribute);
+}
+
+// const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
